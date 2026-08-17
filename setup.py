7871dae0ad4d8502456bb53fa7f240ece6559d7f
sneha jagtap patil <sapatil2115@gmail.com>
2026-08-17 17:05:12 +0530

    update file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="holiday_management",
+    version="0.1.0",
+    author="$neha patil",
+    author_email="sapatil2115@gmail.com"
+    packages=find_packages()
+)
