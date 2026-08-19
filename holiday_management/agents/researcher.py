d974d9e7b805477010bd1fea970e2af1db07a9fd
sneha jagtap patil <sapatil2115@gmail.com>
2026-08-19 13:37:32 +0530

    holiday Researcher

---
+++ holiday_management/agents/researcher.py
@@ -0,0 +1,9 @@
+from autogen_agentchat.agents import AssistantAgent
+from holiday_management.models.gpt_model import model_client  
+
+researcher_agent = AssistantAgent(
+    name="Holiday Researcher",
+    description="a holiday Researcher agent that helps users plan thier trip"
+    model_client=model_client,
+    system_message="you are a travel holiday Researcher agent"
+)
